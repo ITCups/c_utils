46e73ebb5846f63670e0897932b4d91604ed2381
IT_Cups <gurrenlagann54@gmail.com>
2022-01-02 14:30:55 +0700

    updated docs for append

--- documentation/html/search/functions_1.js
+++ documentation/html/search/functions_1.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['ds_5fappend_5fchar_0',['DS_append_char',['../lw__utils_8h.html#aad6dc4600825c2e980c5f5dd74cd489e',1,'lw_utils.c']]],
   ['ds_5ffree_1',['DS_free',['../lw__utils_8h.html#ab2c9940d67f5023e3dee1e9684d99ea1',1,'lw_utils.c']]],
-  ['ds_5frealloc_2',['DS_realloc',['../lw__utils_8h.html#a99184dfdef2c60d8d641fd34247a4741',1,'lw_utils.c']]],
-  ['ds_5fset_5fchar_3',['DS_set_char',['../lw__utils_8h.html#ac5c4339b93a38f56bacced2479d2fdf6',1,'lw_utils.c']]],
-  ['ds_5fset_5ftext_4',['DS_set_text',['../lw__utils_8h.html#a5e969147ba90ae4b21d35255a12f2d78',1,'lw_utils.c']]]
+  ['ds_5finsert_5fchar_2',['DS_insert_char',['../lw__utils_8h.html#a63ecdf36bb3b51b350d98697306dac98',1,'lw_utils.c']]],
+  ['ds_5frealloc_3',['DS_realloc',['../lw__utils_8h.html#a99184dfdef2c60d8d641fd34247a4741',1,'lw_utils.c']]],
+  ['ds_5fset_5fchar_4',['DS_set_char',['../lw__utils_8h.html#ac5c4339b93a38f56bacced2479d2fdf6',1,'lw_utils.c']]],
+  ['ds_5fset_5ftext_5',['DS_set_text',['../lw__utils_8h.html#a5e969147ba90ae4b21d35255a12f2d78',1,'lw_utils.c']]]
 ];
